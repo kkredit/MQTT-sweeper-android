e9d5a555646cfe92fe0c0c0d55672733858c612d
Kevin Kredit <k.kredit.us@ieee.org>
2019-05-28 17:51:03 -0400

    Progress on shodan interface

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/InternetExposureChecker.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/InternetExposureChecker.java
@@ -1,24 +1,64 @@
 package edu.gvsu.cis.mqtt_sweeper;
 
-import android.content.Context;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import com.fooock.shodan.ShodanRestApi;
+import com.fooock.shodan.model.host.Host;
+
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
 
 public class InternetExposureChecker {
 
-    private String API_KEY;
-    private ShodanRestApi api;
+    public interface IEC_Handler {
+        void iecOnComplete();
+        void iecOnError(Throwable e);
+        void iecReceiveAnswer(boolean connected);
+    }
 
-    public InternetExposureChecker() {
-        Context context = Activity.getApplicationContext().getString(R.string.shodan_api_key);
-        api = new ShodanRestApi(API_KEY);
+    private ShodanRestApi m_apiHandle;
+    private IEC_Handler m_handler;
+
+    public InternetExposureChecker(String apiKey, IEC_Handler handler) {
+        m_apiHandle = new ShodanRestApi(apiKey);
+        m_handler = handler;
+    }
+
+    public boolean getExposed() {
+        String ip;
+        try {
+            ip = this.getIp();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        m_apiHandle.hostByIp(false,true, ip)
+            .subscribe(new DisposableObserver<Host>() {
+                @Override
+                public void onComplete() {
+                    m_handler.iecOnComplete();
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    m_handler.iecOnError(e);
+                }
+
+                @Override
+                public void onNext(Host hostReport) {
+                    // TODO: use report to determine if the server is visible
+                    m_handler.iecReceiveAnswer(true);
+                }
+            });
+        return true;
     }
 
     // Credit to https://stackoverflow.com/a/14541376/11407115
-    private static String getIp() throws Exception {
+    public static String getIp() throws Exception {
         URL whatismyip = new URL("http://checkip.amazonaws.com");
         BufferedReader in = null;
         try {
