68259508b95b533e37648724a141f7142e58a066
Kevin Kredit <k.kredit.us@ieee.org>
2019-05-29 13:18:49 -0400

    Shodan scan working; threading scheme not clean

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/InternetExposureChecker.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/InternetExposureChecker.java
@@ -25,16 +25,33 @@ public class InternetExposureChecker {
 
     private ShodanRestApi m_apiHandle;
     private IEC_Handler m_handler;
+    private String m_ipAddr;
 
     InternetExposureChecker(String apiKey, IEC_Handler handler) {
         m_apiHandle = new ShodanRestApi(apiKey);
         m_handler = handler;
     }
 
+    private void setIpAddr(String ip) {
+        m_ipAddr = ip;
+    }
+
+    private String getIpAddr() {
+        return m_ipAddr;
+    }
+
+    private ShodanRestApi getApiHandle() {
+        return m_apiHandle;
+    }
+
+    private IEC_Handler getIecHandler() {
+        return m_handler;
+    }
+
     public boolean getExposed() {
 //        String ip;
         try {
-            new GetIpTask().execute();
+            new GetIpTask().execute(this);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -72,19 +89,21 @@ public class InternetExposureChecker {
         return true;
     }
 
-    private class GetIpTask extends AsyncTask<Void, Integer, String> {
+    private class GetIpTask extends AsyncTask<InternetExposureChecker, Integer, String> {
         protected void onPostExecute(String result) {
             System.out.println("IP address is: " + result);
         }
 
         // Credit to https://stackoverflow.com/a/14541376/11407115
-        protected String doInBackground(Void... voids) {
+        protected String doInBackground(InternetExposureChecker... iecs) {
             BufferedReader in = null;
             try {
                 URL whatismyip = new URL("http://checkip.amazonaws.com");
                 in = new BufferedReader(new InputStreamReader(
                         whatismyip.openStream()));
                 String ip = in.readLine();
+                iecs[0].setIpAddr(ip);
+                new GetShodanScan().execute(iecs);
                 return ip;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -97,6 +116,47 @@ public class InternetExposureChecker {
                 }
                 return null;
             }
+        }
+    }
+
+    private class GetShodanScan extends AsyncTask<InternetExposureChecker, Integer, Void> {
+        protected void onPostExecute(Boolean result) {
+            System.out.println("Is this IP addr hosting MQTT services? " + (result ? "YES" : "NO"));
+        }
+
+        // Credit to https://stackoverflow.com/a/14541376/11407115
+        protected Void doInBackground(InternetExposureChecker... iecs) {
+            InternetExposureChecker iec = iecs[0];
+            IEC_Handler handler = iec.getIecHandler();
+            iec.getApiHandle().hostByIp(false,true, iec.getIpAddr())
+                    .subscribe(new DisposableObserver<Host>() {
+                        @Override
+                        public void onComplete() {
+                            handler.iecOnComplete();
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            handler.iecOnError(e);
+                        }
+
+                        @Override
+                        public void onNext(Host hostReport) {
+                            boolean hasUnencrypted = false;
+                            boolean hasEncrypted = false;
+
+                            for (int port : hostReport.getPorts()) {
+                                hasUnencrypted |= (PORT_NO_TLS == port);
+                                hasEncrypted |= (PORT_TLS == port);
+                            }
+
+                            System.out.println("IEC answer:");
+                            System.out.println("\thasUnencrypted: " + (hasUnencrypted ? "TRUE" : "FALSE"));
+                            System.out.println("\thasEncrypted: " + (hasEncrypted ? "TRUE" : "FALSE"));
+                            handler.iecReceiveAnswer(hasUnencrypted || hasEncrypted);
+                        }
+                    });
+            return null;
         }
     }
 }
