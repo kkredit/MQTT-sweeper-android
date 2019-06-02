90c675f0d91eb4f54ac1cf379ed5f883580da931
Kevin Kredit <k.kredit.us@ieee.org>
2019-06-02 13:29:27 -0400

    Update title of and add menu to broker page

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/BrokerActivity.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/BrokerActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -45,6 +47,23 @@ public class BrokerActivity extends AppCompatActivity {
         m_broker = BrokerContent.ITEM_MAP.get(brokerId);
 
         updateFields();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_logout) {
+            Intent intent = new Intent(BrokerActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return false;
     }
 
     private void updateFields() {
