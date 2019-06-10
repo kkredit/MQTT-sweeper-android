b97e3b5b2d3f0d95fd9a28905336bffcda8bf92c
brinjenga <briannjenga413@gmail.com>
2019-06-10 19:18:02 -0400

    connect to server

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AddBrokerActivity.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AddBrokerActivity.java
@@ -2,13 +2,53 @@ package edu.gvsu.cis.mqtt_sweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class AddBrokerActivity extends AppCompatActivity {
+
+    static String MQTTHOST ;
+    static String USERNAME ;
+    static String PASSWORD ;
+    String topic;
+    MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_broker);
+
+        String clientId = MqttClient.generateClientId();
+       client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+
+
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(AddBrokerActivity.this,"Connected",Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                   Toast.makeText(AddBrokerActivity.this,"Connection failed",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
 
 
     }
