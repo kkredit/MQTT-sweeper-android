02d691b22ba05f912be44298a2455291f233177f
brinjenga <briannjenga413@gmail.com>
2019-06-10 19:41:07 -0400

    publish method

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AddBrokerActivity.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AddBrokerActivity.java
@@ -3,6 +3,7 @@ package edu.gvsu.cis.mqtt_sweeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -11,6 +12,9 @@ import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class AddBrokerActivity extends AppCompatActivity {
 
@@ -54,7 +58,16 @@ public class AddBrokerActivity extends AppCompatActivity {
         } catch (MqttException e) {
             e.printStackTrace();
         }
-
-
     }
+
+    public void publishTopic(View v) {
+        String topic = "bulbs/sittingRoom";
+        String payload = "on";
+        try {
+            client.publish(topic, payload.getBytes(),0,false);
+        } catch ( MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
