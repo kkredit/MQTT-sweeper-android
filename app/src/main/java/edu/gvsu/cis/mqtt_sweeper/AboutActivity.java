7a3b530f6382c89b1404d4dbfe6c0734f1b7be0e
Kevin Kredit <k.kredit.us@ieee.org>
2019-06-18 01:50:06 -0400

    Fill in about screen

--- app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AboutActivity.java
+++ app/src/main/java/edu/gvsu/cis/mqtt_sweeper/AboutActivity.java
@@ -1,13 +1,24 @@
 package edu.gvsu.cis.mqtt_sweeper;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.github_kevin) Button mButtonGithubKevin;
+    @BindView(R.id.github_brian) Button mButtonGithubBrian;
+    @BindView(R.id.button_wiki) Button mButtonMqttWiki;
+    @BindView(R.id.button_priv_policy) Button mButtonPrivPolicy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +26,35 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mButtonGithubKevin.setText(getString(R.string.kevin_github));
+        mButtonGithubBrian.setText(getString(R.string.brian_github));
+        mButtonMqttWiki.setText(getString(R.string.read_more_on_wikipedia));
+        mButtonPrivPolicy.setText(getString(R.string.priv_policy_online));
+    }
+
+    @OnClick(R.id.github_kevin)
+    void onClickGithubKevin() {
+        launchUri("https://github.com/kkredit");
+    }
+
+    @OnClick(R.id.github_brian)
+    void onClickGithubBrian() {
+        launchUri("https://github.com/Brinjenga");
+    }
+
+    @OnClick(R.id.button_wiki)
+    void onClickMqttWiki() {
+        launchUri("https://en.wikipedia.org/wiki/MQTT");
+    }
+
+    @OnClick(R.id.button_priv_policy)
+    void onClickPrivPolicy() {
+        launchUri("tbd");
+    }
+
+    private void launchUri(String uri) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Uri.decode(uri)));
+        startActivity(browserIntent);
     }
 }
